b400467fa80caf72acb58787799343209b2e3eec
nitroclik <reimon1972@gmail.com>
2017-05-01 20:55:02 +0200

    pipe domseguro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { SinfotoPipe } from './pipes/sinfoto.pipe';
 import { ArtistaComponent } from './components/artista/artista.component';
+
+
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +26,8 @@ import { ArtistaComponent } from './components/artista/artista.component';
     SearchComponent,
     NavbarComponent,
     SinfotoPipe,
-    ArtistaComponent
+    ArtistaComponent,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule,
